349a7b172ab659a49a43ca6c99c06c67d7045b7f
Ritesh <riteshtiwari3112003@gmail.com>
2025-07-17 13:16:23 +0530

    i have added bout state in clasw component

--- class_based_component/index.js
+++ class_based_component/index.js
@@ -28,4 +28,26 @@ root.render(<ClassComponent/>);
  * const root=ReactDOM.createRoot(document.getElementById("root"));
  * root.render(<ClassComponent/>);
  * 
+ * 
+ * like example is i have a simple html file like in same folder index.html have 
+ * and javascript file like 
+ * class ClassComponent extends React.Component{
+ * constructor(props){
+ * super(props);
+ * this.state="mother";
+ * this.handleClick=this.handleClick.bind(this);}
+ * handleClick(){
+ * this.setState("father");//  but here error come becuse setState takes an object }
+ * render(){
+ * return(
+ * <div>
+ * <h1>I love my {this.state}</h1>
+ * <button onClick={this.handleClick}>Chsnge Love</button>
+ * </div>)}}
+ * 
+ * so update like this like 
+ * this.state={"person":"mother"};
+ * 
+ * and <h1> i love my {this.state.person}</h1>
+ * and this.setState({"person":"father"});
  */
