cb1055ba917968ae717e8c2a41d14908104ad6b0
Ritesh <riteshtiwari3112003@gmail.com>
2025-07-24 16:54:42 +0530

    i have update some notes on conditional component

--- controlled_compoent/index.js
+++ controlled_compoent/index.js
@@ -22,3 +22,14 @@ class ControlledComponent extends React.Component{
 }
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(<ControlledComponent/>);
+
+/**what is the controlled component 
+ * in class based React, a controled component is a form element (like <input > <textarea> or <select> ) the form element does not manage its own state 
+ * instead react does via this.state and this.setState method 
+ * every change in the input goies through an onChange handler that update the state 
+ * 
+ * why use controlled component ?
+ * you can validate input as the user type 
+ * you can conditionally disable and enable button 
+ * you have full contro over form 's behaviourr 
+ */
