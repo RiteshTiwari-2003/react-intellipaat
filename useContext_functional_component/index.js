2c5f31f3156c911d8c1b45eab5730569de5950ea
Ritesh <riteshtiwari3112003@gmail.com>
2025-08-01 23:36:17 +0530

    i have completed my use context hook note

--- useContext_functional_component/index.js
+++ useContext_functional_component/index.js
@@ -35,3 +35,45 @@ function Level2Child(){
         </div>
     );
 }
+function Level3Child(){
+    console.log("Level3Child rendered");
+    return(
+        <div>
+            <h3>Level3Child rendered</h3>
+            <Level4Child/>
+        </div>
+    );
+}
+function Level4Child(){
+    console.log("Level4Child rendered");
+    return(
+        <div>
+            <h4>Level4Child rendered</h4>
+            <Level5Child/>
+        </div>
+    );
+}
+function Level5Child(){
+    console.log("Level5Child rendered");
+    return(
+        <div>
+            <h5>Level5Child rendered</h5>
+            <Level6Child/>
+        </div>
+    )
+
+}
+function Level6Child(){
+    console.log("Level6Child rendered");
+    const data=React.useContext(ParentStateContext);
+
+    return(
+        <div>
+            <h1>Level6Child rendered</h1>
+            <h2>Parent state received via usecontext:{data}</h2>
+
+        </div>
+    );
+}
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<ParentContext/>);
